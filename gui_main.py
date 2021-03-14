ba71bc640cb0a80e2f75160401f45f71b556c87a
dsersun <d.sersun@gmail.com>
2021-03-14 15:26:57 +0200

    Начал создавать версию с Графическим интерфейсом

---
+++ gui_main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title('Test')
+root.geometry('800x500')
+root.resizable(width=False, height=False)     # <---
+
+root.mainloop()
+
+
+print("Hello World!")
